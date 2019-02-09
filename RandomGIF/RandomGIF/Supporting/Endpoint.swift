1c4d3997e4831fb2854e9d005f5362c5d2e91bd6
Kevin Chan <kevinchan159@gmail.com>
2019-02-09 00:48:23 -0500

    Fix indentation

--- RandomGIF/RandomGIF/Supporting/Endpoint.swift
+++ RandomGIF/RandomGIF/Supporting/Endpoint.swift
@@ -9,93 +9,93 @@ import Foundation
 
 
 enum EndpointMethod: String {
-	case get = "GET"
-	case post = "POST"
-	case delete = "DELETE"
+    case get = "GET"
+    case post = "POST"
+    case delete = "DELETE"
 }
 
 struct Endpoint {
-	static var apiVersion: Int? = 1
+    static var apiVersion: Int? = 1
 
-	let path: String
-	let queryItems: [URLQueryItem]
-	let headers: [String: String]
-	let body: Data?
-	let method: EndpointMethod
+    let path: String
+    let queryItems: [URLQueryItem]
+    let headers: [String: String]
+    let body: Data?
+    let method: EndpointMethod
 }
 
 extension Endpoint {
-	/// General initializer
-	init<T: Codable>(path: String, queryItems: [URLQueryItem] = [], headers: [String: String] = [:], body: T? = nil, method: EndpointMethod = .get) {
-		self.path = path
-		self.queryItems = queryItems
-		self.headers = headers
-		self.method = (body != nil) ? .post : method
-		self.body = try? JSONEncoder().encode(body)
-	}
+    /// General initializer
+    init<T: Codable>(path: String, queryItems: [URLQueryItem] = [], headers: [String: String] = [:], body: T? = nil, method: EndpointMethod = .get) {
+        self.path = path
+        self.queryItems = queryItems
+        self.headers = headers
+        self.method = (body != nil) ? .post : method
+        self.body = try? JSONEncoder().encode(body)
+    }
 
-	/// POST initializer
-	init<T: Codable>(path: String, headers: [String: String] = [:], body: T) {
-		self.path = path
-		self.queryItems = []
-		self.headers = headers
-		self.method = .post
+    /// POST initializer
+    init<T: Codable>(path: String, headers: [String: String] = [:], body: T) {
+        self.path = path
+        self.queryItems = []
+        self.headers = headers
+        self.method = .post
 
-		//Encode body
-		self.body = try? JSONEncoder().encode(body)
-	}
+        //Encode body
+        self.body = try? JSONEncoder().encode(body)
+    }
 
-	/// GET initializer
-	init(path: String, queryItems: [URLQueryItem] = [], headers: [String: String] = [:]) {
-		self.path = path
-		self.queryItems = queryItems
-		self.headers = headers
-		self.method = .get
-		self.body = nil
-	}
+    /// GET initializer
+    init(path: String, queryItems: [URLQueryItem] = [], headers: [String: String] = [:]) {
+        self.path = path
+        self.queryItems = queryItems
+        self.headers = headers
+        self.method = .get
+        self.body = nil
+    }
 }
 
 extension Endpoint {
-	// We still have to keep 'url' as an optional, since we're
-	// dealing with dynamic components that could be invalid.
-	var url: URL? {
-		var components = URLComponents()
-		components.scheme = "http"
-		components.host = "api.giphy.com"
-		components.path = path
-		components.queryItems = queryItems
-		return components.url
-	}
+    // We still have to keep 'url' as an optional, since we're
+    // dealing with dynamic components that could be invalid.
+    var url: URL? {
+        var components = URLComponents()
+        components.scheme = "http"
+        components.host = "api.giphy.com"
+        components.path = path
+        components.queryItems = queryItems
+        return components.url
+    }
 
 
-	var urlRequest: URLRequest? {
-		guard let unwrappedURL = url else { return nil }
-		var request = URLRequest(url: unwrappedURL)
-		headers.forEach { (key, value) in
-			request.addValue(value, forHTTPHeaderField: key)
-		}
-		request.httpMethod = method.rawValue
-		request.httpBody = body
+    var urlRequest: URLRequest? {
+        guard let unwrappedURL = url else { return nil }
+        var request = URLRequest(url: unwrappedURL)
+        headers.forEach { (key, value) in
+            request.addValue(value, forHTTPHeaderField: key)
+        }
+        request.httpMethod = method.rawValue
+        request.httpBody = body
 
-		return request
-	}
+        return request
+    }
 }
 
 struct APIResponse: Codable {
-	var data: GiphyResponse
+    var data: GiphyResponse
 }
 
 struct GiphyResponse: Codable {
-	var title: String
-	var image_original_url: String
+    var title: String
+    var image_original_url: String
 }
 
 extension Endpoint {
 
-	static func getRandomGIF() -> Endpoint {
-		let apiKey = "cT7TWQzcmtSwAsBnrZ92WLJXwvaPdbFV"
-		let apiKeyQueryItem = URLQueryItem(name: "api_key", value: apiKey)
-		return Endpoint(path: "/v1/gifs/random", queryItems: [apiKeyQueryItem])
-	}
+    static func getRandomGIF() -> Endpoint {
+        let apiKey = "cT7TWQzcmtSwAsBnrZ92WLJXwvaPdbFV"
+        let apiKeyQueryItem = URLQueryItem(name: "api_key", value: apiKey)
+        return Endpoint(path: "/v1/gifs/random", queryItems: [apiKeyQueryItem])
+    }
 
 }
--- RandomGIF/RandomGIF/ViewController.swift
+++ RandomGIF/RandomGIF/ViewController.swift
@@ -10,109 +10,109 @@ import UIKit
 
 class ViewController: UIViewController {
 
-	var titleLabel: UILabel!
-	var gifImageView: UIImageView!
-	var newGIFButton: UIButton!
+    var titleLabel: UILabel!
+    var gifImageView: UIImageView!
+    var newGIFButton: UIButton!
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		self.title = "Random GIF"
-		view.backgroundColor = .white
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Random GIF"
+        view.backgroundColor = .white
 
-		setupViews()
-		getRandomGIF()
-	}
+        setupViews()
+        getRandomGIF()
+    }
 
-	// MARK: - Initialize and layout views
-	func setupViews() {
-		// Setup UIImageView which will be used to display the GIF
-		gifImageView = UIImageView()
-		gifImageView.translatesAutoresizingMaskIntoConstraints = false
-		gifImageView.contentMode = .scaleAspectFit
-		view.addSubview(gifImageView)
+    // MARK: - Initialize and layout views
+    func setupViews() {
+        // Setup UIImageView which will be used to display the GIF
+        gifImageView = UIImageView()
+        gifImageView.translatesAutoresizingMaskIntoConstraints = false
+        gifImageView.contentMode = .scaleAspectFit
+        view.addSubview(gifImageView)
 
-		NSLayoutConstraint.activate([
-			gifImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-			gifImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-			gifImageView.widthAnchor.constraint(equalToConstant: 300),
-			gifImageView.heightAnchor.constraint(equalToConstant: 300)
-			])
+        NSLayoutConstraint.activate([
+            gifImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            gifImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            gifImageView.widthAnchor.constraint(equalToConstant: 300),
+            gifImageView.heightAnchor.constraint(equalToConstant: 300)
+            ])
 
-		// Setup a label:
-		// - has centered text
-		// - has blue background color
-		// - has white text color
-		// - font size of text is 20
-		// - has 5 pixel rounded corners
-		titleLabel = UILabel()
-		titleLabel.translatesAutoresizingMaskIntoConstraints = false
-		titleLabel.textAlignment = .center
-		titleLabel.backgroundColor = .blue
-		titleLabel.textColor = .white
-		titleLabel.font = UIFont.systemFont(ofSize: 20)
-		titleLabel.layer.cornerRadius = 5
-		view.addSubview(titleLabel)
+        // Setup a label:
+        // - has centered text
+        // - has blue background color
+        // - has white text color
+        // - font size of text is 20
+        // - has 5 pixel rounded corners
+        titleLabel = UILabel()
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.textAlignment = .center
+        titleLabel.backgroundColor = .blue
+        titleLabel.textColor = .white
+        titleLabel.font = UIFont.systemFont(ofSize: 20)
+        titleLabel.layer.cornerRadius = 5
+        view.addSubview(titleLabel)
 
-		NSLayoutConstraint.activate([
-			titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-			titleLabel.bottomAnchor.constraint(equalTo: gifImageView.topAnchor, constant: -30)
-			])
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: gifImageView.topAnchor, constant: -30)
+            ])
 
-		// Setup a button:
-		// - whose title is "Get new GIF!"
-		// - has red background color
-		// - whose title color is red
-		// - title has a font size of 20
-		// - has 5 pixel rounded corners
-		// - when tapped on:
-		//    - changes the background color of this button to red
-		//    - fetches a new GIF from Giphy API
-		//    - updates the text of the titleLabel to be the title of the gif
-		newGIFButton = UIButton()
-		newGIFButton.translatesAutoresizingMaskIntoConstraints = false
-		newGIFButton.setTitle("Get new GIF!", for: .normal)
-		newGIFButton.backgroundColor = .red
-		newGIFButton.setTitleColor(.black, for: .normal)
-		newGIFButton.titleLabel?.font = .systemFont(ofSize: 20)
-		newGIFButton.layer.cornerRadius = 5
-		newGIFButton.addTarget(self, action: #selector(newGIFButtonPressed), for: .touchUpInside)
-		view.addSubview(newGIFButton)
+        // Setup a button:
+        // - whose title is "Get new GIF!"
+        // - has red background color
+        // - whose title color is red
+        // - title has a font size of 20
+        // - has 5 pixel rounded corners
+        // - when tapped on:
+        //    - changes the background color of this button to red
+        //    - fetches a new GIF from Giphy API
+        //    - updates the text of the titleLabel to be the title of the gif
+        newGIFButton = UIButton()
+        newGIFButton.translatesAutoresizingMaskIntoConstraints = false
+        newGIFButton.setTitle("Get new GIF!", for: .normal)
+        newGIFButton.backgroundColor = .red
+        newGIFButton.setTitleColor(.black, for: .normal)
+        newGIFButton.titleLabel?.font = .systemFont(ofSize: 20)
+        newGIFButton.layer.cornerRadius = 5
+        newGIFButton.addTarget(self, action: #selector(newGIFButtonPressed), for: .touchUpInside)
+        view.addSubview(newGIFButton)
 
-		NSLayoutConstraint.activate([
-			newGIFButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-			newGIFButton.topAnchor.constraint(equalTo: gifImageView.bottomAnchor, constant: 50)
-			])
-	}
+        NSLayoutConstraint.activate([
+            newGIFButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            newGIFButton.topAnchor.constraint(equalTo: gifImageView.bottomAnchor, constant: 50)
+            ])
+    }
 
-	@objc func newGIFButtonPressed() {
-		// Set the newGIFButton background color back to red
-		newGIFButton.backgroundColor = .red
-		getRandomGIF()
-	}
+    @objc func newGIFButtonPressed() {
+        // Set the newGIFButton background color back to red
+        newGIFButton.backgroundColor = .red
+        getRandomGIF()
+    }
 
-	// MARK: - GET A RANDOM GIF FROM GIPHY API
-	func getRandomGIF() {
-		let networking = URLSession.shared.request
-		let future = networking(Endpoint.getRandomGIF()).decode(APIResponse.self)
-		future.observe { (result) in
-			switch result {
-			case .value(let apiResponse):
-				DispatchQueue.main.async {
-					// Get the image URL from the Giphy API and then convert it to a UIImage
-					let gifImage = UIImage.gifImageWithURL(apiResponse.data.image_original_url)
-					self.gifImageView.image = gifImage
+    // MARK: - GET A RANDOM GIF FROM GIPHY API
+    func getRandomGIF() {
+        let networking = URLSession.shared.request
+        let future = networking(Endpoint.getRandomGIF()).decode(APIResponse.self)
+        future.observe { (result) in
+            switch result {
+            case .value(let apiResponse):
+                DispatchQueue.main.async {
+                    // Get the image URL from the Giphy API and then convert it to a UIImage
+                    let gifImage = UIImage.gifImageWithURL(apiResponse.data.image_original_url)
+                    self.gifImageView.image = gifImage
 
-					// Set the titleLabel's text to the title of the gif from Giphy API
-					self.titleLabel.text = apiResponse.data.title
+                    // Set the titleLabel's text to the title of the gif from Giphy API
+                    self.titleLabel.text = apiResponse.data.title
 
-					// Set the newGIFButton background color back to green
-					self.newGIFButton.backgroundColor = .green
-				}
-			case .error(let error):
-				print(error)
-			}
-		}
-	}
+                    // Set the newGIFButton background color back to green
+                    self.newGIFButton.backgroundColor = .green
+                }
+            case .error(let error):
+                print(error)
+            }
+        }
+    }
 
 }
 
